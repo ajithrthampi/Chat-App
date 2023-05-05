39698dcf300845b0ca89434c0cceeb6dd2dfe490
phozpho <ajithrthampi999@gmail.com>
2023-05-05 23:43:28 +0530

    Added logo

--- my-project/src/components/Messages.jsx
+++ my-project/src/components/Messages.jsx
@@ -14,6 +14,7 @@ const Messages = () => {
   const {data} = useContext(ChatContext)
 
   useEffect(() => {
+    
     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
        doc.exists() && setMessages(doc.data().messages)
     })
